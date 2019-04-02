ab8eb2617d3784bbd0f847797d81b823ea5db3a1
Stuix <abomdonoman@gmail.com>
2019-04-02 11:57:03 +0200

    Initial commit

---
+++ src/main/java/com/pvtgrupp8/monto/MontoApplication.java
@@ -0,0 +1,13 @@
+package com.pvtgrupp8.monto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MontoApplication.class, args);
+    }
+
+}
