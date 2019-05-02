ee50ee5a40316a2016b824ed7d3580815bf405f0
gustavjunedahl <gustav.junedahl@gmail.com>
2019-05-02 15:10:03 +0200

    Edited entity to reflex new db schema

--- src/main/java/com/pvtgrupp8/monto/entities/Rating.java
+++ src/main/java/com/pvtgrupp8/monto/entities/Rating.java
@@ -14,12 +14,79 @@ public class Rating {
     @Column(name="rating")
     private double rating;
 
-    @ManyToOne
+    @ManyToOne(
+        cascade={CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH}
+    )
+    @JoinColumn(name="user_id")
     private User ratingCreator;
+
+    @Column(name = "comment")
     private String comment;
 
-
-    @ManyToOne
+    @ManyToOne(
+        cascade={CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH}
+    )
     @JoinColumn(name="route_id")
     private Route route;
+
+    public Rating() {}
+
+    public Rating(double rating, User ratingCreator, String comment,
+        Route route) {
+        this.rating = rating;
+        this.ratingCreator = ratingCreator;
+        this.comment = comment;
+        this.route = route;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public User getRatingCreator() {
+        return ratingCreator;
+    }
+
+    public void setRatingCreator(User ratingCreator) {
+        this.ratingCreator = ratingCreator;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+            "id=" + id +
+            ", rating=" + rating +
+            ", ratingCreator=" + ratingCreator +
+            ", comment='" + comment + '\'' +
+            ", route=" + route +
+            '}';
+    }
 }
