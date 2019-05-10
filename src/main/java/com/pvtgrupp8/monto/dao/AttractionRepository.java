31554c0c3b66dc8bcbc39cc396b2d0de5cd8157f
gustavjunedahl <gustav.junedahl@gmail.com>
2019-05-10 10:31:58 +0200

    Projection for attraction

--- src/main/java/com/pvtgrupp8/monto/dao/AttractionRepository.java
+++ src/main/java/com/pvtgrupp8/monto/dao/AttractionRepository.java
@@ -1,13 +1,28 @@
 package com.pvtgrupp8.monto.dao;
 
 import com.pvtgrupp8.monto.entities.Attraction;
+import com.pvtgrupp8.monto.entities.Creator;
+import com.pvtgrupp8.monto.entities.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 
 import java.util.List;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.Projection;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@Projection(name = "inlinePosition", types = { Attraction.class })
+interface InlinePosition {
+    int getId();
+    String getDescription();
+    String getTitle();
+    String getTitleEnglish();
+    String getPicture();
+    Position getPosition();
+    List<Creator> getCreators();
+}
+
+@CrossOrigin(origins = "http://localhost:8100")
+@RepositoryRestResource(excerptProjection = InlinePosition.class)
 public interface AttractionRepository extends JpaRepository<Attraction, Integer> {
 
    // @Query("from Attraction where title like %?1")
